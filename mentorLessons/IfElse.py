1706a03717758a46350b1878c2b49cb816f603ad
vusalbilalov <vusalbilalov@yahoo.com>
2026-07-20 15:03:41 +0400

    Update

---
+++ mentorLessons/IfElse.py
@@ -0,0 +1,46 @@
+# yas=46
+#
+# if yas>46:
+#     print("Size teqaud dusur")
+
+# temparute=int(input("Zehmet olmasa dereceni qeyd ederdiniz"))
+#
+# if temparute>35:
+#     print("Hava cox istidir")
+# else:
+#     print("Hava cox soyuqdur")
+
+
+# anniversary=80
+#
+# if anniversary >= 60:
+#     print("Diamond")
+# elif anniversary > 70:
+#     print("Platinium")
+# elif anniversary >= 50:
+#     print("Golden")
+# else:
+#     print("Just anniversary")
+
+# sechki_huququ=26
+#
+# if  sechki_huququ>25:
+#     print("sechki huququ var")
+#     print("Deputatliga namized ola biler")
+# elif sechki_huququ>=18:
+#     print("sechki huququ var")
+# else:
+#     print("Hec bir huququ yoxdur")
+
+# yetkindirmi=False
+#
+# if yetkindirmi:  #==True
+#     print("Ses vere biler")
+# if not(yetkindirmi): #==False
+#     print("Ses vere bilmez")
+
+mesaj="mene komek huququ lazimdir"
+
+if "salam" in mesaj:
+    print("Buyrun size komek ede bilerik")
+
