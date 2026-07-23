8ebf3f03a910e60786d0cee0d36118c18cb89145
vusalbilalov <vusalbilalov@yahoo.com>
2026-07-23 13:01:57 +0400

    Update

---
+++ mentorLessons/ListTuple.py
@@ -0,0 +1,102 @@
+# adlar=["Tural","Nicat","Amin"]
+#
+# adlar.sort(reverse=True)
+# print(adlar)
+
+"Tural" #0
+"Taleh" #2 and #-1
+
+# print(adlar[1])
+# print(adlar[-1])
+
+# adlar.append("Ilham")
+# print(adlar)
+
+# adlar.remove("Taleh")
+# print(adlar)
+
+# adlar.pop(0)
+# print(adlar)
+
+# adlar.clear()
+# print(adlar)
+
+# adlar.insert(2,"Ilham")
+# print(adlar)
+
+
+# adlar[0]="Rovshan"
+# print(adlar)
+
+
+# reqemler=[2,6,4,9,7,8]
+
+# reqemler.pop()
+# print(reqemler)
+
+# reqemler.insert(4,6)
+# print(reqemler)
+
+# reqemler.pop(2)
+# print(reqemler)
+# reqemler.sort(reverse=True)
+# print(reqemler)
+
+# adlar=["Tural","Nicat","Amin","Tural","Nicat","Amin","Tural","Nicat","Amin"]
+#
+#
+# print(len(adlar))
+
+
+# meyveler=["alma","kivi","avakado","armud","banan","gavali","portagal","heyva"]
+
+# print(meyveler[3:6])
+# print(meyveler[3:])
+# print(meyveler[:3])
+# print(meyveler[-3:])
+# print(meyveler[:-2])
+# print(meyveler[::-1])
+
+
+# adlar=["eli","veli","tural"]
+#
+# boyuk_adlar =[ad.upper() for ad in adlar ]
+# print(boyuk_adlar)
+
+
+# reqemler=[1,2,3,4,5]
+#
+# ikiqatlar=[]
+#
+# for i in reqemler:
+#     ikiqatlar.append(i*2)
+# print(ikiqatlar)
+
+
+meyveler1=["alma","armud","heyva","nar","kivi","banan","mango","avakado"]  #nested-list
+meyveler2=["alma","armud","heyva","nar","kivi","banan","mango","avakado"]
+meyveler3=["alma","armud","heyva","nar","kivi","banan","mango","avakado"]
+
+
+# meyve_ref =[
+# ["alma","armud","heyva","nar","kivi","banan","mango","avakado"],
+# ["alma","armud","heyva","nar","kivi","limon","mango","guava"],
+# ["alma","armud","heyva","nar","kivi","banan","mango","avakado"]
+# ]
+#
+# print(meyve_ref[1][-1])
+
+tuple=("Vusal","1996","Baki")
+tuple2=("Murad","1996","Baki")
+
+# print.append("Murad")
+
+# list_s1=list(tuple)
+# print(list_s1)
+
+tuple3=tuple+tuple2
+print(tuple3)
+
+
+
+
